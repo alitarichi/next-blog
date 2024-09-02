245ddbf7a8e93f7a2324e775f622dfa23faf77ca
ali tarichi <tarichiali@gmail.com>
2024-09-02 18:08:15 +0300

    added category in the sidebar

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -66,6 +66,30 @@ const Menu = () => {
           </div>
         </Link>
       </div>
+
+      <div>
+        <h2 className={styles.subtitle}>Discover by topic</h2>
+        <h1 className={styles.detail}>Categories</h1>
+        <Link href="/" className={`${styles.categoryItem} ${styles.style}`}>
+          Style
+        </Link>
+        <Link href="/" className={`${styles.categoryItem} ${styles.culture}`}>
+          Culture
+        </Link>
+        <Link href="/" className={`${styles.categoryItem} ${styles.food}`}>
+          Food
+        </Link>
+        <Link href="/" className={`${styles.categoryItem} ${styles.fashion}`}>
+          Fashion
+        </Link>
+        <Link href="/" className={`${styles.categoryItem} ${styles.travel}`}>
+          Travel
+        </Link>
+        <Link href="/" className={`${styles.categoryItem} ${styles.coding}`}>
+          Coding
+        </Link>
+      </div>
+
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.detail}>Editor's Pick</h1>
       <div className={styles.items}>
