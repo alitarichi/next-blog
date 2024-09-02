cd5e83ec7261424b144dbfe00ba79afc20698e2f
ali tarichi <tarichiali@gmail.com>
2024-09-02 13:04:39 +0300

    card list and card added

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
+import Card from "../card/Card";
 const CardList = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>RecentPosts</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
       <Pagination />
     </div>
   );
