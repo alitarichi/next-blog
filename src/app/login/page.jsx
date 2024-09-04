ffa70f77b5c1e7528788e5ac0ebfd56ff548d39d
ali tarichi <tarichiali@gmail.com>
2024-09-04 13:37:07 +0300

    loginpage inital commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./loginPage.module.css";
+
+const login = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}></div>
+    </div>
+  );
+};
+
+export default login;
