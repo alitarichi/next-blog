a6748e95d9028c5398426880ce0864b5005f4e71
ali tarichi <tarichiali@gmail.com>
2024-09-04 15:28:38 +0300

    react quill added

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,8 +1,44 @@
+"use client";
+
+import Image from "next/image";
 import styles from "./writePage.module.css";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
 
 const WritePage = () => {
-  return <div className={styles.container}>WritePage</div>;
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <div className={styles.container}>
+      <input type="text" placeholder="Title" />
+      <div className={styles.editor}>
+        <button className={styles.button}>
+          <Image src="/plus.png" alt="plus" width={16} height={16} />
+        </button>
+        {open && (
+          <div className={styles.add}>
+            <button className={styles.addButton}>
+              <Image src="/image.png" alt="plus" width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <Image src="/external.png" alt="plus" width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <Image src="/video.png" alt="plus" width={16} height={16} />
+            </button>
+          </div>
+        )}
+        <ReactQuill
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your Story ..."
+        />
+      </div>
+    </div>
+  );
 };
 
-export default Write;
-Page;
+export default WritePage;
