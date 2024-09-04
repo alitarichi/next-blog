3ce19f09ce8308131c42c472c5ea53475c91e915
ali tarichi <tarichiali@gmail.com>
2024-09-04 12:33:18 +0300

    comments section implanted

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,7 +1,48 @@
+import Link from "next/link";
 import styles from "./commnets.module.css";
+import Image from "next/image";
 
 const Comments = () => {
-  return <div className={styles.container}>Comments</div>;
+  const status = "authenticated";
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === "authenticated" ? (
+        <div className={styles.write}>
+          <textarea
+            placeholder="write a comment ..."
+            className={styles.input}
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </div>
+      ) : (
+        <Link href={"/login"}>login to write a comment</Link>
+      )}
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt="user"
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}>10.03.2022</span>
+            </div>
+          </div>
+          <p className={styles.content}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
