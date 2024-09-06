033e271f322669613ea2d8b9c94ced3d966ba68c
ali tarichi <tarichiali@gmail.com>
2024-09-06 16:28:27 +0300

    FETCHING DATA FROM POSTS IN THE DATABASE WORKS

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,16 +2,29 @@ import React from "react";
 import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Card from "../card/Card";
-const CardList = () => {
+
+const getData = async (page) => {
+  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CardList = async ({ page }) => {
+  const data = await getData(page);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>RecentPosts</h1>
       <div className={styles.posts}>
         <div className={styles.post}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {data?.map((item) => (
+            <Card item={item} key={item._id} />
+          ))}
         </div>
       </div>
       <Pagination />
