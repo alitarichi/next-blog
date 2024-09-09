56c876047f8b57d32aaf74837f8af096e225a6fb
ali tarichi <tarichiali@gmail.com>
2024-09-09 16:37:32 +0300

    tried to fetch the commets after posting a new comment but it doesn't post for some reason

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,10 +1,11 @@
 "use client";
 
 import Link from "next/link";
-import styles from "./commnets.module.css";
+import styles from "./comments.module.css";
 import Image from "next/image";
-import { useSession } from "next-auth/react";
 import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -21,34 +22,49 @@ const fetcher = async (url) => {
 
 const Comments = ({ postSlug }) => {
   const { status } = useSession();
-  const { data, isLoading } = useSWR(
+
+  const { data, mutate, isLoading } = useSWR(
     `http://localhost:3000/api/comments?postSlug=${postSlug}`,
     fetcher
   );
+
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = async () => {
+    await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+    mutate();
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
       {status === "authenticated" ? (
         <div className={styles.write}>
           <textarea
-            placeholder="write a comment ..."
+            placeholder="write a comment..."
             className={styles.input}
-          ></textarea>
-          <button className={styles.button}>Send</button>
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       ) : (
-        <Link href={"/login"}>login to write a comment</Link>
+        <Link href="/login">Login to write a comment</Link>
       )}
       <div className={styles.comments}>
         {isLoading
-          ? "loading ..."
+          ? "loading"
           : data?.map((item) => (
               <div className={styles.comment} key={item._id}>
                 <div className={styles.user}>
                   {item?.user?.image && (
                     <Image
                       src={item.user.image}
-                      alt="user"
+                      alt=""
                       width={50}
                       height={50}
                       className={styles.image}
@@ -56,9 +72,7 @@ const Comments = ({ postSlug }) => {
                   )}
                   <div className={styles.userInfo}>
                     <span className={styles.username}>{item.user.name}</span>
-                    <span className={styles.date}>
-                      {item.createdAt.substring(0, 10)}
-                    </span>
+                    <span className={styles.date}>{item.createdAt}</span>
                   </div>
                 </div>
                 <p className={styles.desc}>{item.desc}</p>
