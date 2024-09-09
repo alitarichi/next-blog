ff3478167757e6ae0a63676906236ced71b1f201
ali tarichi <tarichiali@gmail.com>
2024-09-09 12:43:44 +0300

    every post fetching works

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,58 +1,58 @@
-import React from "react";
-import styles from "./singlePage.module.css";
 import Menu from "@/components/menu/Menu";
+import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 
-const page = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image
-                src={"/p1.jpeg"}
-                alt="post"
-                fill
-                className={styles.avatar}
-              />
-            </div>
+            {data?.user?.image && (
+              <div className={styles.userImageContainer}>
+                <Image
+                  src={data.user.image}
+                  alt="user"
+                  fill
+                  className={styles.avatar}
+                />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}>10.03.2022</span>
+              <span className={styles.username}>{data?.user.name}</span>
+              <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <Image src={"/p1.jpeg"} alt="post" fill className={styles.image} />
-        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="post" fill className={styles.image} />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit.
-            </p>
-            <h2>Lorem ipsum dolor sit amet,</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
-          <div className={styles.comments}>
-            <Comments />
+          <div className={styles.description}>{data?.desc}</div>
+          <div className={styles.comment}>
+            <Comments postSlug={slug} />
           </div>
         </div>
         <Menu />
@@ -61,4 +61,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default SinglePage;
